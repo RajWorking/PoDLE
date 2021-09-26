689938811f8fb0762f21a6ab11f9ec29f9654986
RajWorking <rajmaheshwari08@yahoo.com>
2021-09-26 12:51:15 +0545

    Started work

---
+++ Sources/podle/main.swift
@@ -0,0 +1,21 @@
+import Foundation
+import Crypto
+SHA256.hash(data: Data("inputString".utf8))
+
+class PoDLE {
+
+    func generate_podle() -> [String: String] {
+        return ["abcd":"ab"]
+    }
+    
+    func exec() -> String {
+        "PRint this"
+    }
+
+    // funct
+
+    // func get_commitment() -> []
+}
+
+let b = PoDLE()
+print("\(b.exec())")
